827b6bcabea368ecdf1705a3ba62f55d5b17e322
superkotik666 <102909986+superkotik666@users.noreply.github.com>
2023-05-24 23:11:52 +0300

    Update web.php
    add new routs

--- routes/web.php
+++ routes/web.php
@@ -4,27 +4,13 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/', [\App\Http\Controllers\IndexController::class, 'index'])->name('home');
+Route::get('/login', [\App\Http\Controllers\AuthController::class, 'showLoginForm'])->name('login');
+Route::post('/login_process', [\App\Http\Controllers\AuthController::class, 'login'])->name('login_process');
+Route::get('/register', [\App\Http\Controllers\AuthController::class, 'showRegisterForm'])->name('register');
+Route::post('/register_process', [\App\Http\Controllers\AuthController::class, 'register'])->name('register_process');
+Route::get('/forum', [\App\Http\Controllers\AuthController::class, 'forum'])->name('forum');
+Route::middleware("auth:web")->group(function () {
+    Route::get('/logout', [\App\Http\Controllers\AuthController::class, 'logout'])->name('logout');
 
-Route::get('/', function () {
-    return view('welcome');
-})->name('home');;
-
-Route::middleware("guest:web")->group(function () {
-    Route::get('/login', [\App\Http\Controllers\AuthController::class, 'showLoginForm'])->name('login');
-    Route::post('/login_process', [\App\Http\Controllers\AuthController::class, 'login'])->name('login_process');
-
-    Route::get('/register', [\App\Http\Controllers\AuthController::class, 'showRegisterForm'])->name('register');
-    Route::post('/register_process', [\App\Http\Controllers\AuthController::class, 'register'])->name('register_process');
-
-   
 });
