18e44b612dbe689d42b6da24757c873db117be62
superkotik666 <102909986+superkotik666@users.noreply.github.com>
2023-05-31 23:39:16 +0300

    Add PostController

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Events\CommentCreated;
+use App\Http\Requests\CommentForm;
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+
+
+class PostController extends Controller
+{
+    public function index(){
+        
+        $posts = Post::orderBy("created_at", "DESC")->paginate(3);
+        
+        
+            return view('posts.index', [
+                "posts" => $posts,
+            ]);
+     }
+
+
+     public function show($id)
+     {
+         $post = Post::findOrFail($id);
+ 
+         return view('posts.show', [
+             "post" => $post,
+         ]);
+     }
+
+
+    }
